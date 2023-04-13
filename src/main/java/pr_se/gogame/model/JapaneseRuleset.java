1db10c879944f8e3cd9ffe2526e974fb89c39fe7
Sebastian <115575444+sewaUni@users.noreply.github.com>
2023-04-13 22:22:34 +0200

    draw basis for Ruleset

--- src/main/java/pr_se/gogame/model/JapaneseRuleset.java
+++ src/main/java/pr_se/gogame/model/JapaneseRuleset.java
@@ -1,4 +1,18 @@
 package pr_se.gogame.model;
 
 public class JapaneseRuleset implements Ruleset {
+    @Override
+    public void setSuicide(boolean allow) {
+
+    }
+
+    @Override
+    public void setKoAmount(int amount) {
+
+    }
+
+    @Override
+    public void scoreGame() {
+
+    }
 }
--- src/main/java/pr_se/gogame/model/Ruleset.java
+++ src/main/java/pr_se/gogame/model/Ruleset.java
@@ -1,4 +1,39 @@
 package pr_se.gogame.model;
 
 public interface Ruleset {
+
+    /** Depending on the rules, suicide is allowed or forbidden. <br>
+     * -> set to true if you want to allow it <br>
+     * -> set to false if you want to forbid it <br>
+     * @param allow (true or false)
+     */
+    void setSuicide(boolean allow);
+
+    /** Depending on the rules, suicide is allowed or forbidden. This is the default option to forbid suicide. <br>
+     * @return false
+     */
+    default boolean setDefaultSuicide() {
+        return false;
+    }
+
+    /** To prevent endless repetitions or make them pointless, positional repetition is restricted. <br>
+     * This method sets the amount of restricted repetitions.
+     * @param amount (2 ... n)
+     */
+    void setKoAmount(int amount);
+
+    /** To prevent endless repetitions or make them pointless, positional repetition is restricted. <br>
+     * Two consecutive moves (except passing) may not restore the original position.
+     * @return 2
+     */
+    default int setDefaultKoAmount() {
+        return 2;
+    }
+
+    /** Evaluation is the central feature of a set of rules; It varies depending on the set of rules.
+     *
+     */
+    void scoreGame();
+
+    //Kompensationspunkte
 }
