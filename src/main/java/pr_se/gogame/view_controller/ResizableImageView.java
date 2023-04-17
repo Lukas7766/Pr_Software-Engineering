51fa0e95e1612d452f40e4503bbc859347fbee89
Gerald <122382776+k12119624@users.noreply.github.com>
2023-04-17 14:32:21 +0200

    document ResizableImageView

--- src/main/java/pr_se/gogame/view_controller/ResizableImageView.java
+++ src/main/java/pr_se/gogame/view_controller/ResizableImageView.java
@@ -4,6 +4,9 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 /**
+ * This subclass of ImageView - unlike said superclass - can be automatically resized by its parents like other nodes
+ * can.
+ *
  * Inspired by <a href="https://www.reddit.com/r/JavaFX/comments/l2yzwg/resize_imageview_to_fit_parent_pane/">...</a>
  */
 public class ResizableImageView extends ImageView {
