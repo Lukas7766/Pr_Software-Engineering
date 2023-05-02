6656b2b0f81b85dbe78996916bb49f80ac5862b5
Gerald <122382776+k12119624@users.noreply.github.com>
2023-05-02 14:27:19 +0200

    update label color in showMoveNumber()
    Otherwise, the label appears to stay the opposite color of the center of the background tile, rather than the currently set stone.

--- src/main/java/pr_se/gogame/view_controller/BoardPane.java
+++ src/main/java/pr_se/gogame/view_controller/BoardPane.java
@@ -34,7 +34,7 @@ public class BoardPane extends GridPane {
     /**
      * whether move numbers are shown on the stones
      */
-    private boolean showsMoveNumbers = true;
+    private boolean showsMoveNumbers = false;
 
     /**
      * whether coordinates are shown on the sides of the board
@@ -640,6 +640,7 @@ public class BoardPane extends GridPane {
          */
         public void showMoveNumber() {
             LABEL.setVisible(CURRENTLY_SET_STONE != null && showsMoveNumbers);
+            updateLabelColor();
         }
 
         /**
