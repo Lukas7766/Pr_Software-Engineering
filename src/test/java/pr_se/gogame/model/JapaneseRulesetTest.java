bec0c7fadb21a4bd2a046584c1a0c09c3707b917
Sebastian <115575444+sewaUni@users.noreply.github.com>
2023-05-08 22:24:47 +0200

    Standardregelwerk #10 documentation + basic testing structure

--- src/test/java/pr_se/gogame/model/JapaneseRulesetTest.java
+++ src/test/java/pr_se/gogame/model/JapaneseRulesetTest.java
@@ -15,52 +15,62 @@ class JapaneseRulesetTest {
     }
 
     @Test
+    @DisplayName("suicide is not allowed in JP ruleset")
     void getSuicide() {
-        assertTrue(japaneseRuleset.getSuicide(null));
+        assertFalse(japaneseRuleset.getSuicide(null));
     }
 
     @Test
-    @DisplayName("allowed repeatable moves should be 1")
+    @DisplayName("allowed repeatable moves are one in JP ruleset")
     void getKoAmount() {
         assertEquals(1, japaneseRuleset.getKoAmount());
     }
 
     @Test
+    @DisplayName("default handicap placement is active")
     void hasDefaultHandicapPlacement() {
         assertTrue(japaneseRuleset.hasDefaultHandicapPlacement());
     }
 
     @Test
     void setHandicapStones() {
+        assertTrue(false);
     }
 
     @Test
+    @DisplayName("6.5 komi points were added to player white")
     void getKomi() {
         assertEquals(6.5, japaneseRuleset.getKomi());
     }
 
     @Test
     void predicateKoMove() {
+        assertTrue(false);
     }
 
     @Test
     void getKoMove() {
+        assertTrue(false);
     }
 
     @Test
     void resetKoMove() {
+        assertTrue(false);
     }
 
     @Test
     void scoreGame() {
+        assertTrue(false);
     }
 
     @Test
     void calculateTerritoryPoints() {
+        assertTrue(false);
     }
 
     @Test
     void floodFill() {
+        assertTrue(false);
     }
 
 }
