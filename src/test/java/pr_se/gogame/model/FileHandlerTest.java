64b92a2f8c09349e7aa6a9a8da6dab07bd287ad6
Gerald <122382776+k12119624@users.noreply.github.com>
2023-06-24 20:23:02 +0200

    Update FileHandlerTest.java

--- src/test/java/pr_se/gogame/model/FileHandlerTest.java
+++ src/test/java/pr_se/gogame/model/FileHandlerTest.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import static pr_se.gogame.model.StoneColor.*;
 
-public class FileHandlerTest {
+class FileHandlerTest {
     Game game;
 
     @BeforeEach
