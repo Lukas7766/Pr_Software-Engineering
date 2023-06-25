f74d4309234e9d12761ba8336a91b9f8ff3f0af3
Gerald <122382776+k12119624@users.noreply.github.com>
2023-06-25 15:55:48 +0200

    disable debug mode

--- src/main/java/pr_se/gogame/view_controller/GlobalSettings.java
+++ src/main/java/pr_se/gogame/view_controller/GlobalSettings.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 public final class GlobalSettings {
-    public static final boolean DEBUG = true;
+    public static final boolean DEBUG = false;
 
     private static boolean confirmationNeeded = false;
 
