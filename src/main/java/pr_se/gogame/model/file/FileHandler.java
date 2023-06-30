0faeec8f4302f81d0484ce3c03fdb1784dc4f456
Gerald <122382776+k12119624@users.noreply.github.com>
2023-06-30 20:06:51 +0200

    Update FileHandler.java

--- src/main/java/pr_se/gogame/model/file/FileHandler.java
+++ src/main/java/pr_se/gogame/model/file/FileHandler.java
@@ -4,9 +4,27 @@ import java.io.File;
 import java.nio.file.NoSuchFileException;
 
 public interface FileHandler {
+    /**
+     * Saves a game to the supplied file
+     * @param file The file to be saved
+     * @return True if saving succeeded, false otherwise
+     */
     boolean saveFile(File file);
 
+    /**
+     * Loads a game from the supplied file
+     * @param file The file to be loaded
+     * @return True if loading succeeded, false otherwise
+     * @throws NoSuchFileException If the file to be loaded does not exist
+     * @throws LoadingGameException If a content-related error occurred while loading the game, mostly due to
+     *  unsupported features
+     */
     boolean loadFile(File file) throws NoSuchFileException, LoadingGameException;
 
+    /**
+     * Returns the currently set file.
+     * @return The file that this FileHandler currently saves to, null if no file has been set (i.e., loaded or saved)
+     *  yet.
+     */
     File getCurrentFile();
 }
